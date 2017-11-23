babaf72f3dcd4322be778366b193a543c450d375
mcroni <joeydanieldarko@gmail.com>
2017-11-23 22:20:38 +0000

    virgin commit 3

--- kwese/settings.py
+++ kwese/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'kwese.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL ='/media/'

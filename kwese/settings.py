493572df432461d685a5fdeb7131d87c43a46743
mcroni <joeydanieldarko@gmail.com>
2017-11-23 22:59:01 +0000

    virgin commit 4

--- kwese/settings.py
+++ kwese/settings.py
@@ -14,8 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = MEDIA_DIR
+print(STATIC_URL)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# print(STATIC_ROOT)
 MEDIA_URL ='/media/'
